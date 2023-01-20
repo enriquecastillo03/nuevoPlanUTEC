b399e3793221f1b305d69c74d2ecb1d2ceb20b37
Enrique Castillo <enriquecastillo@GrupoRavicorp-TI.local>
2023-01-20 00:02:53 -0600

    Modificacion 1 de prueba

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 /*
  *---------------------------------------------------------------
- * APPLICATION ENVIRONMENT
+ * APPLICATION ENVIRONMENT Ok
  *---------------------------------------------------------------
  *
  * You can load different configurations depending on your
